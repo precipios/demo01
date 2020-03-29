2e14e758b9d566638b0530445b03407090502dd8
Dave Reynolds <dave@precipios.com>
2020-03-29 11:34:19 -0400

    First commit

---
+++ R/linest.R
@@ -0,0 +1,15 @@
+
+# This is a function that will return an object
+linest <- function(x, y) {
+  # x is the x-data
+  # y is the y-data
+  model <- lm(y~x)
+
+  return(model)
+}
+
+# # Example:
+# x <- c(1,2,3,4,5)
+# y <- c(5,6,4,2,1)
+# mod <- lm(y~x)
+# summary(mod)
